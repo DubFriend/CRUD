c9f9a0c0e897912fd6f108c03d1c61b81d7b210a
Brian Detering <bdeterin@gmail.com>
2013-11-01 23:58:46 -0700

    using mysql database on backend

--- crud.php
+++ crud.php
@@ -42,8 +42,13 @@
 ini_set('display_errors', 1);
 error_reporting(E_STRICT|E_ALL);
 
-require 'jsonStore.php';
-$file = new jsonStore('data');
+require 'sequel.php';
+
+$sql = new Sequel(new PDO(
+    'mysql:dbname=crud_demo;host=localhost',
+    'root',
+    'P0l.ar-B3ar'
+));
 
 function getID() {
     return explode('/', $_SERVER['PATH_INFO'])[1];
@@ -53,36 +58,98 @@ function getPageNO() {
     return explode('/', $_SERVER['PATH_INFO'])[2];
 }
 
-function limit(array $allRows, $begining, $numberOfResults) {
-    $limited = array();
-    for(
-        $i = $begining;
-        $i < $begining + $numberOfResults && $i < count($allRows);
-        $i += 1
-    ) {
-        $limited[] = $allRows[$i];
-    }
-    return $limited;
-}
-
 $requestData = json_decode(file_get_contents('php://input'), true);
 $rowsPerPage = 3;
-$numberOfPages = ceil(count($file->select()) / $rowsPerPage);
+
+function doesStartWith($start, $string) {
+    return substr($string, 0, strlen($start)) === $start;
+}
+
+//example: array('order_a' => 3, 'filter_b' => 'foo') -> array('a' => 3, 'b' => 'foo')
+function stripLabelFromKeys($startsWith, array $array) {
+    $stripped = array();
+    foreach($array as $key => $value) {
+        $stripped[substr($key, strlen($startsWith))] = $value;
+    }
+    return $stripped;
+}
+
+function getParameters($startsWith, array $request) {
+    $parameters = array();
+    foreach($request as $key => $value) {
+        if(doesStartWith($startsWith, $key)) {
+            $parameters[$key] = $value;
+        }
+    }
+    return stripLabelFromKeys($startsWith, $parameters);
+}
+
+function buildOrderBySQL(array $request) {
+    $orders = array_filter(getParameters('order_', $request), function ($direction) {
+        return $direction !== "neutral";
+    });
+    $orderSQL = array();
+    foreach($orders as $column => $direction) {
+        if($direction === 'ascending') {
+            $orderSQL[] = $column . ' ASC';
+        }
+        else if($direction === 'descending') {
+            $orderSQL[] = $column . ' DESC';
+        }
+        else {
+            throw new Exception(
+                '"order_" GET variables must by of values ' .
+                '"ascending", "descending", or "neutral"'
+            );
+        }
+    }
+    return count($orderSQL) > 0 ? ' ORDER BY ' . implode(', ', $orderSQL) : '';
+}
+
+function buildWhereSQL(array $request) {
+    $filters = array_filter(getParameters('filter_', $request), function ($value) {
+        return $value !== '';
+    });
+    $filtersSQL = array();
+    foreach($filters as $name => $value) {
+        switch($name) {
+            case 'Maximum_Awesome':
+                $filtersSQL[] = 'awesome <= ' . $value;
+                break;
+            case 'Search_Textarea':
+                $filtersSQL[] = 'textarea LIKE %' . $value . '%';
+                break;
+            case 'fruit':
+                if(is_array($value)) {
+                    $filterSQL[] = implode(',', $value);
+                }
+                else {
+                    throw new Exception('Checkboxes should return an array.');
+                }
+                break;
+            default:
+                throw new Exception('Invalid filter keyname "' . $name . '".');
+        }
+    }
+    return count($filters) > 0 ? ' WHERE ' . implode(' AND ', $filtersSQL) : '';
+}
 
 $response = null;
 switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $page = getPageNO();
+        $pageNumber = getPageNO();
+        $results = $sql->query(
+            "SELECT * FROM crud " .
+            buildWhereSQL($_GET) . ' ' .
+            buildOrderBySQL($_GET) . ' ' .
+            'LIMIT ' . (($pageNumber - 1) * $rowsPerPage) . ', ' . $rowsPerPage
+        );
         $response = array(
-            'pages' => $numberOfPages,
-            'data' => limit(
-                $file->select(),
-                $rowsPerPage * ($page - 1),
-                $rowsPerPage
-            ));
+            'pages' => ceil($results->count() / $rowsPerPage),
+            'data' => $results->toArray()
+        );
         break;
     case 'PUT':
-        $file->update($requestData, array('id' => getID()));
         if($requestData['letter'] === 'a') {
             http_response_code(409);
             $response = array(
@@ -91,15 +158,16 @@ switch($_SERVER['REQUEST_METHOD']) {
             );
         }
         else {
-            $response = true;
+            $requestData['fruit'] = implode(',', $requestData['fruit']);
+            $response = $sql->update('crud', $requestData, array('id' => getID()));
         }
         break;
     case 'POST':
-        $response = $file->insert($_POST);
+        $_POST['fruit'] = implode(',', $_POST['fruit']);
+        $response = $sql->insert('crud', $_POST);
         break;
     case 'DELETE':
-        $file->delete(array('id' => getID()));
-        $response = true;
+        $response = $sql->delete('crud', array('id' => getID()));
         break;
     default:
         throw new Exception("Invalid Request Method");
